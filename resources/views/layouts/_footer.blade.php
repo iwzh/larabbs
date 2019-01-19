13d809b875eecb88ca481a5a063227a076343e2e
wzh <wzhec@foxmail.com>
2019-01-19 17:27:08 +0800

    base layout
    Signed-off-by: wzh <wzhec@foxmail.com>

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="http://wangzenghui.com" target="_blank">WZH</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:wzhec@foxmail.com">联系我们</a></p>
+  </div>
+</footer>
